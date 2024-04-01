6af2e40f5871c1f0f91233dcdd8d9a43515893f8
Javed006 <shaik.javed@seabed2crest.com>
2024-04-01 18:03:36 +0530

    portDetails with portName or portCode endpoint

--- src/main/java/com/customs/network/fdapn/service/ExcelParser.java
+++ src/main/java/com/customs/network/fdapn/service/ExcelParser.java
@@ -1,6 +1,9 @@
 package com.customs.network.fdapn.service;
 
 import com.customs.network.fdapn.model.PortCodeDetails;
+import com.customs.network.fdapn.repository.PortCodeDetailsRepository;
+import io.micrometer.common.util.StringUtils;
+import org.apache.poi.ss.formula.functions.T;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
@@ -12,7 +15,14 @@ import java.util.List;
 
 @Component
 public class ExcelParser {
-    public static List<PortCodeDetails> parse(MultipartFile file) throws IOException {
+    private final PortCodeDetailsRepository repository;
+
+    public ExcelParser(PortCodeDetailsRepository repository) {
+        this.repository = repository;
+    }
+
+    //LATER WILL REMOVE THIS IS FOR PORT-DETAILS ENTRIES
+    public List<PortCodeDetails> parse(MultipartFile file) throws IOException {
         List<PortCodeDetails> portCodeDetailsList = new ArrayList<>();
         try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
             Sheet sheet = workbook.getSheetAt(0);
@@ -24,16 +34,32 @@ public class ExcelParser {
                 portCodeDetails.setSno((long) row.getCell(0).getNumericCellValue());
                 portCodeDetails.setCountry(row.getCell(1).getStringCellValue());
                 portCodeDetails.setState(row.getCell(2).getStringCellValue());
-                portCodeDetails.setPortName(row.getCell(3).getStringCellValue());
+                if (row.getCell(3) != null) {
+                    portCodeDetails.setPortName(row.getCell(3).getStringCellValue());
+                } else {
+                    portCodeDetails.setPortName(null);
+                    System.out.println("Row with Serial Number " + portCodeDetails.getSno() + " has a null PortName.");
+                }
+
                 Cell portCodeCell = row.getCell(4);
                 if (portCodeCell != null && portCodeCell.getCellType() == CellType.NUMERIC) {
                     portCodeDetails.setPortCode((int) portCodeCell.getNumericCellValue());
                 } else {
                     portCodeDetails.setPortCode(null);
+                    System.out.println("Row with Serial Number " + portCodeDetails.getSno() + " has a null PortCode.");
                 }
                 portCodeDetailsList.add(portCodeDetails);
             }
         }
         return portCodeDetailsList;
+    }
+    public List<?> getPortDetailsByPortNumberOrPortName(String portName, Integer portCode) {
+        List<?> portDetails;
+        if(!StringUtils.isBlank(portName)){
+            portDetails = repository.findDistinctPortNamesByPattern(portName);
+        } else {
+            portDetails = repository.findDistinctPortCodesByPattern(portCode);
+        }
+        return portDetails;
     }
 }
