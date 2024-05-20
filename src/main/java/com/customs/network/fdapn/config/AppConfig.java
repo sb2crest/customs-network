f415a38d864c3e1a7c5bdc9d55e23b23da359d5f
Akshay <Courses@seabed2crest.com>
2024-05-20 11:35:53 +0530

    done restTemplate config changes

--- src/main/java/com/customs/network/fdapn/config/AppConfig.java
+++ src/main/java/com/customs/network/fdapn/config/AppConfig.java
@@ -1,7 +1,11 @@
 package com.customs.network.fdapn.config;
 
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +13,13 @@ public class AppConfig {
     
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setMaxRedirects(50)
+                .build();
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(factory);
     }
 }
